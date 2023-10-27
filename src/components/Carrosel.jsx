f788bc3ee4f838f77f60f13bac39413f3ecc03ba
vinicius costa <vitonilcius@gmail.com>
2023-10-27 00:12:24 -0300

    criação da sessão depoimentos

---
+++ src/components/Carrosel.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import style from '../styles/Player.module.scss';
+
+export default function Carrosel({ videoNames }){
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === 0 ? videoNames.length - 1 : prevIndex - 1));
+    };
+    
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex === videoNames.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    const anterior = "<";
+    const proximo = ">";
+    
+    return (
+        <div className={style.container}>
+            <button className={style.anterior} onClick={handlePrev}>{anterior}</button>
+            <video className={style.video} src={`/${videoNames[currentIndex]}`} controls width="500" height="300" />
+            <button className={style.proximo} onClick={handleNext}>{proximo}</button>
+        </div>
+    )
+}
--- src/components/estrutura/Depoimentos.jsx
+++ src/components/estrutura/Depoimentos.jsx
@@ -1,8 +1,20 @@
+//import depoimento1 from '../../../public/depoimento1.mp4'
+import Carrosel from '../Carrosel';
+import React from 'react';
+import style from '../../styles/Depoimentos.module.scss';
+
+const videoNames = [
+    'depoimento1.mp4',
+    'depoimento2.mp4',
+    'depoimento3.mp4'
+];
+
 export default function Depoimentos(){
+
     return (
-        <div>
-            <h1>Depoimentos</h1>
-            <video src="" controls></video>
+        <div className={style.fundo}>
+            <p className={style.titulo}>Veja o Depoimento de quem confia em OliveAGE®</p>
+            <Carrosel videoNames={videoNames} />
         </div>
     )
 }
