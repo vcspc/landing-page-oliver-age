46f1c775e78a564dc118723cb912f00f9cfc5bde
vinicius costa <vitonilcius@gmail.com>
2023-11-14 13:28:40 -0300

    Final

--- src/components/estrutura/Depoimentos.jsx
+++ src/components/estrutura/Depoimentos.jsx
@@ -2,6 +2,7 @@
 import Carrosel from '../Carrosel';
 import React from 'react';
 import style from '../../styles/Depoimentos.module.scss';
+/* import styles from '../../styles/Player.module.scss'; */
 
 const videoNames = [
     'depoimento1.mp4',
@@ -14,7 +15,13 @@ export default function Depoimentos(){
     return (
         <div className={style.fundo}>
             <p className={style.titulo}>Veja o Depoimento de quem confia em OliveAGE®</p>
-            <Carrosel videoNames={videoNames} />
+            <div className={style.container}>
+                
+                <video className={style.video} src="/depoimento1.mp4" controls />
+                <video className={style.video} src="/depoimento2.mp4" controls />
+                <video className={style.video} src="/depoimento3.mp4" controls />
+            </div>
+            {/* <Carrosel videoNames={videoNames} /> */}
         </div>
     )
 }
